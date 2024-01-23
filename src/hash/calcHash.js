c3bcac4f7b862396b3a34aaeaf915a30491ee7af
Yevhenii Burkovskyi <yevhenii.burkovskyi@stormotion.io>
2024-01-23 23:02:38 +0200

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const hash = crypto.createHash("sha256");
+
+  const readibleStream = fs.createReadStream(
+    path.join(__dirname, "files", "fileToCalculateHashFor.txt"),
+    {
+      encoding: "utf-8",
+    }
+  );
+
+  readibleStream.on("data", (chunk) => hash.update(chunk));
+
+  readibleStream.on("end", () => console.log(hash.digest("hex")));
 };
 
 await calculateHash();
