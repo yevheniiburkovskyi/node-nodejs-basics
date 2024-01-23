8ad4c51d1b5a6d8c4bb02e8759b28c41ca325865
Yevhenii Burkovskyi <yevhenii.burkovskyi@stormotion.io>
2024-01-23 21:43:24 +0200

    feat: complete cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+
+  for (let i = 2; i < args.length; i += 2) {
+    const propName = args[i].slice(2);
+    const value = args[i + 1];
+
+    console.log(`${propName} is ${value}`);
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  console.log(process.env);
+
+  Object.keys(process.env).map((item) =>
+    console.log(`RSS_${item}=${process.env[item]}`)
+  );
 };
 
 parseEnv();
