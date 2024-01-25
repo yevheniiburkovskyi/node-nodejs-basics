42e2887801b2701afa01d62284014ad4ad4a0f3e
Yevhenii Burkovskyi <yevhenii.burkovskyi@stormotion.io>
2024-01-25 21:27:50 +0200

    feat: complete worker tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import os from "os";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const workerPath = path.join(__dirname, "worker.js");
+  const numberOfWorkers = os.cpus().length;
+
+  const results = [];
+
+  for (let i = 0; i < numberOfWorkers; i++) {
+    const worker = new Worker(workerPath);
+
+    worker.postMessage(i + 10);
+
+    worker.on("message", (message) => {
+      results[i] = message;
+      if (results.filter((result) => result).length === numberOfWorkers) {
+        console.log(results);
+
+        process.exit();
+      }
+    });
+  }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,21 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = (result) => {
+  parentPort.postMessage(result);
+  // This function sends result of nthFibonacci computations to main thread
 };
+
+parentPort.on("message", (data) => {
+  const res = nthFibonacci(data);
+  sendResult({ status: "resolved", data: res });
+});
+
+parentPort.on("messageerror", () => {
+  sendResult({ status: "error", data: null });
+});
 
 sendResult();
