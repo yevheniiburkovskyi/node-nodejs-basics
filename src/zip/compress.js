5cc37ac553b33bd7b535fbf31b9df3f5f5ba690c
Yevhenii Burkovskyi <yevhenii.burkovskyi@stormotion.io>
2024-01-25 20:07:28 +0200

    feat: complete zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+
+import { pipeline } from "stream";
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const sourcePath = path.join(__dirname, "files", "fileToCompress.txt");
+  const targetPath = path.join(__dirname, "files", "archive.gz");
+
+  const gzip = createGzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(targetPath);
+
+  source.pipe(gzip).pipe(destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+
+import { pipeline } from "stream";
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const sourcePath = path.join(__dirname, "files", "archive.gz");
+  const targetPath = path.join(__dirname, "files", "fileToCompress.txt");
+
+  const gzip = createGunzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(targetPath);
+
+  source.pipe(gzip).pipe(destination);
 };
 
 await decompress();
